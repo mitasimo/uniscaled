ed2ee0fcd60cf1fb00b2365a74d0a58696b0aa6a
Dmitry Simonenko <dmitry.simonenko@gmail.com>
2021-02-10 10:12:25 +0300

    add comment  to sigcontext.go

--- sigcontext.go
+++ sigcontext.go
@@ -6,7 +6,7 @@ import (
 	"os/signal"
 )
 
-// WithSignal -
+// WithSignal creates a context that canceled when a signals is received
 func WithSignal(parentCtx context.Context, s ...os.Signal) (context.Context, context.CancelFunc) {
 	cancelContext, cancelFunc := context.WithCancel(parentCtx)
 	signalContext := &signalContext{Context: cancelContext}
@@ -14,10 +14,13 @@ func WithSignal(parentCtx context.Context, s ...os.Signal) (context.Context, con
 	return context.Context(signalContext), func() { cancelFunc() }
 }
 
+//
 type signalContext struct {
-	context.Context
+	context.Context // parent context
 }
 
+// processSignal starts a goroutine that terminates
+// the context when the OS receives a signal
 func (c *signalContext) processSignal(cancel context.CancelFunc, s ...os.Signal) {
 	chanSig := make(chan os.Signal, 1)
 	signal.Notify(chanSig, s...)
